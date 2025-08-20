4f1ecdf4a59e81f348f53933495e895febc5923c
Eliel <100846616+eliel2107@users.noreply.github.com>
2025-08-20 17:23:45 -0300

    Refactor to use static ObjectMapper

--- src/main/java/br/com/alura/tabelafipe/service/ConverteDados.java
+++ src/main/java/br/com/alura/tabelafipe/service/ConverteDados.java
@@ -1,18 +1,20 @@
 package br.com.alura.tabelafipe.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
 
 import java.util.List;
 
 public class ConverteDados implements IConverteDados {
-    private ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper MAPPER = new ObjectMapper()
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
         try {
-            return mapper.readValue(json, classe);
+            return MAPPER.readValue(json, classe);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
@@ -20,12 +22,12 @@ public class ConverteDados implements IConverteDados {
 
     @Override
     public <T> List<T> obterLista(String json, Class<T> classe) {
-        CollectionType lista = mapper.getTypeFactory()
+        CollectionType lista = MAPPER.getTypeFactory()
                 .constructCollectionType(List.class, classe);
 
 
         try {
-            return mapper.readValue(json, lista);
+            return MAPPER.readValue(json, lista);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
